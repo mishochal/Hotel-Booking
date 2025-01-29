6c51a238b2ed63cd0989fa61066ae59c2e58ac48
Misho Chaladze <mishochaladze4@gmail.com>
2025-01-29 15:37:59 +0400

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { BookedRoomsComponent } from './booked-rooms/booked-rooms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "home", component: HomeComponent },
+    { path: "hotels", component: HotelsComponent },
+    { path: "rooms", component: RoomsComponent },
+    { path: "booked-rooms", component: BookedRoomsComponent }
+];
