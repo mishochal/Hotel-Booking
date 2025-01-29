48b66b6dcc96d3fc25b878ed10895b7e7a3a6978
Misho Chaladze <mishochaladze4@gmail.com>
2025-01-29 15:38:52 +0400

    add routing in hero component

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-hero',
-  imports: [],
-  templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+    selector: 'app-hero',
+    imports: [RouterModule],
+    templateUrl: './hero.component.html',
+    styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
 
