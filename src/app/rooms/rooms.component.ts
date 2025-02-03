a7b6a15f01315142d6e7a54bdfe47b8cceb9b380
Misho Chaladze <mishochaladze4@gmail.com>
2025-02-03 14:18:46 +0400

    add http requests for rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomsService } from '../services/rooms.service';
+import { Room } from '../models/rooms.model';
+import { RoomComponent } from '../shared/room/room.component';
+import { CommonModule } from '@angular/common';
+import { FilterComponent } from '../shared/filter/filter.component';
+import { Filter } from '../models/filter.model';
 
 @Component({
-  selector: 'app-rooms',
-  imports: [],
-  templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.scss'
+    selector: 'app-rooms',
+    imports: [CommonModule, RoomComponent, FilterComponent],
+    templateUrl: './rooms.component.html',
+    styleUrl: './rooms.component.scss'
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
+    rooms: Room[] = [];
+    roomTypes: Filter[] = [];
 
+    constructor(private roomsService: RoomsService) { }
+
+    ngOnInit(): void {
+        this.getAllRooms();
+        this.getRoomTypes();
+    }
+
+    getAllRooms(): void {
+        this.roomsService.getAllRooms().subscribe(
+            (rooms) => {
+                this.rooms = rooms;
+            }
+        )
+    }
+
+    getRoomTypes(): void {
+        this.roomsService.getFillters().subscribe(
+            (types) => {
+                this.roomTypes = types;
+            }
+        )
+    }
 }
--- src/app/services/rooms.service.ts
+++ src/app/services/rooms.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from '../models/rooms.model';
+import { Filter } from '../models/filter.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RoomsService {
 
-  constructor() { }
+    private apiUrl = "https://hotelbooking.stepprojects.ge/api/Rooms";
+
+    constructor(private http: HttpClient) { }
+
+    getAllRooms(): Observable<Room[]> {
+        const url = `${this.apiUrl}/GetAll`;
+        return this.http.get<Room[]>(url);
+    }
+
+    getFillters(): Observable<Filter[]> {
+        const url = `${this.apiUrl}/GetRoomTypes`;
+        return this.http.get<Filter[]>(url);
+    }
 }
