dc1a0a1d053030f7de3609de6d67cd5873e91ef4
Misho Chaladze <mishochaladze4@gmail.com>
2025-02-17 16:21:17 +0400

    add booking form interface

--- src/app/models/booking.model.ts
+++ src/app/models/booking.model.ts
@@ -1,11 +1,14 @@
-export interface BookedRoom {
+export interface BookedRoom extends BookingForm {
     id: number;
+    totalPrice: number;
+    isConfirmed: boolean;
+}
+
+export interface BookingForm {
     roomID: number;
     checkInDate: string;
     checkOutDate: string;
-    totalPrice: number;
-    isConfirmed: boolean;
     customerName: string;
-    custmerId: string;
+    customerId: string;
     customerPhone: string;
 }
