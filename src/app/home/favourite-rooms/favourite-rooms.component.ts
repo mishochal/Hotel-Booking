eb4f5cda5470355cd7e5d63f54cc8dada810ee73
Misho Chaladze <mishochaladze4@gmail.com>
2025-02-24 14:28:43 +0400

    make favourite rooms dynamic instead of hard-coded

--- src/app/home/favourite-rooms/favourite-rooms.component.ts
+++ src/app/home/favourite-rooms/favourite-rooms.component.ts
@@ -1,12 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Room } from '../../models/rooms.model';
+import { RoomsService } from '../../services/rooms.service';
+import { take } from 'rxjs';
+import { RoomComponent } from '../../shared/room/room.component';
+import { LoadingSpinnerComponent } from '../../shared/loading-spinner/loading-spinner.component';
 
 @Component({
     selector: 'app-favourite-rooms',
-    imports: [CommonModule],
+    imports: [CommonModule, RoomComponent, LoadingSpinnerComponent],
     templateUrl: './favourite-rooms.component.html',
     styleUrl: './favourite-rooms.component.scss'
 })
-export class FavouriteRoomsComponent {
-    rooms: any[] = new Array(6);
+export class FavouriteRoomsComponent implements OnInit {
+    rooms: Room[] = [];
+    isLoaded: boolean = false;
+
+    constructor(private roomsService: RoomsService) { }
+
+    ngOnInit(): void {
+        this.loadFavouriteRooms();
+    }
+
+    loadFavouriteRooms(): void {
+        this.roomsService.getAllRooms().pipe(
+            take(1),
+        ).
+            subscribe(
+                (rooms) => {
+                    this.rooms = rooms.splice(0, 6);
+                    this.isLoaded = true;
+                }
+            )
+    }
+
 }
